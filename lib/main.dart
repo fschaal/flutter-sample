02bce927a45c39c7aa3fba0b3e3bd206fc8c470b
Florian <f.schaal@caesar.nl>
2020-10-30 11:25:37 +0100

    add final product

--- lib/main.dart
+++ lib/main.dart
@@ -3,3 +3,41 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(App());
 }
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int counter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        title: Text("Demo gegeven $counter"),
+      ),
+      body: Center(child: Text("Dit is echt easy!")),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.red,
+        child: Icon(Icons.add),
+        onPressed: () => {
+          setState(() {
+            counter++;
+          })
+        },
+      ),
+    );
+  }
+}
